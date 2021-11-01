543e7d3ab786f20b1e7f031db4d0963910c341b4
Hshefat <h9shefat@gmail.com>
2021-11-01 11:25:01 +0600

    design upgrade

--- src/main/java/com/sts/controller/EmpController.java
+++ src/main/java/com/sts/controller/EmpController.java
@@ -17,66 +17,79 @@ import com.sts.service.EmpService;
 
 @Controller
 public class EmpController {
-	
+
 	@Autowired
 	private EmpService service;
-	
-	@GetMapping("/")
-	public String home(Model m) {
-		
-		List<Employee > emp = service.getAllEmp();
-		m.addAttribute("emp", emp);
-		return "index";
-	}
-	
-	@GetMapping("addemp")
-	public String addEmpForm() {
-		
-		return "add_emp";
-	}
-	
-	@PostMapping("/register")
-	public String empRegister(@ModelAttribute Employee e, HttpSession session) {
-		
-		System.out.println(e);
-		
-		service.addEmp(e);
-		session.setAttribute("msg", "Employee added successfully. "); 
-		
-		return "redirect:/";
-		
-	}
-	
-	@GetMapping("/edit/{id}")
-	public String edit(@PathVariable int id, Model m) {
-		
-		Employee e = service.getEMpById(id);
-		m.addAttribute("emp",e);
-		
-		return "edit";
-		
-	}
 
 	
 	
-	@PostMapping("/update")
-	public String updateEmp (@ModelAttribute Employee e, HttpSession session) {
-		
-		service.addEmp(e);
-		session.setAttribute("msg", "Employee data updated");
-		
-		return "redirect:/";
-		
-		
+	
+	
+	
+	
+	@GetMapping("/test")
+	public String addTest() {
+		return "test";
 	}
 	
 	
+	
+	
+	
+	
+	
+	
+	@GetMapping("/")
+	public String home(Model m) {
+
+		List<Employee> emp = service.getAllEmp();
+		m.addAttribute("emp", emp);
+		return "index";
+	}
+
+	@GetMapping("addemp")
+	public String addEmpForm() {
+
+		return "add_emp";
+	}
+
+	@PostMapping("/register")
+	public String empRegister(@ModelAttribute Employee e, HttpSession session) {
+
+		System.out.println(e);
+
+		service.addEmp(e);
+		session.setAttribute("msg", "Employee added successfully. ");
+
+		return "redirect:/";
+
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id, Model m) {
+
+		Employee e = service.getEMpById(id);
+		m.addAttribute("emp", e);
+
+		return "edit";
+
+	}
+
+	@PostMapping("/update")
+	public String updateEmp(@ModelAttribute Employee e, HttpSession session) {
+
+		service.addEmp(e);
+		session.setAttribute("msg", "Employee data updated");
+
+		return "redirect:/";
+
+	}
+
 	@GetMapping("/delete/{id}")
 	public String deleteEmp(@PathVariable int id, HttpSession session) {
-		
+
 		service.deleteEmp(id);
-		session.setAttribute("msg","Employee data deleted.");
+		session.setAttribute("msg", "Employee data deleted.");
 		return "redirect:/";
 	}
 }
- 
--- src/main/java/com/sts/service/EmpService.java
+++ src/main/java/com/sts/service/EmpService.java
@@ -1,6 +1,5 @@
 package com.sts.service;
 
-import java.lang.StackWalker.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,19 +13,19 @@ import com.sts.repository.EmpRepo;
 
 @Service
 public class EmpService {
-	
+
 	@Autowired
 	private EmpRepo repo;
 	public void addEmp (Employee e) {
-		
+
 		repo.save(e);
 	}
-	
+
 	public List<Employee> getAllEmp(){
-		
+
 		return repo.findAll();
 	}
-	
+
 	public Employee getEMpById(int id) {
 		Optional<Employee> e = repo.findById(id);
 		if(e.isPresent()) {
@@ -34,7 +33,7 @@ public class EmpService {
 		}
 		return null;
 	}
-	
+
 	public void deleteEmp(int id) {
 		repo.deleteById(id);
 	}
